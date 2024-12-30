df3396210ff84b1b49bf8c7780890cb346b0ccc6
unknown <fabioflauzino@hotmail.com>
2024-12-30 01:05:13 -0300

    Criando header da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { Header } from './components/Header'
+import styles from './app.module.css'
 
 function App() {
 
+  function handleRestartGame() {
+    alert('Restarting game')
+  }
+
   return (
-    <div>
-      <h1>Iniciando APP</h1>
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+      </main>
     </div>
   )
 }
